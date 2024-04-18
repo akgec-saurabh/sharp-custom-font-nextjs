b1e20ecb69a005c2ea995d21068649d86318f675
Saurabh Kumar <akgec.saurabh@gmail.com>
2024-04-19 00:00:38 +0530

    watermark

--- app/components/random.tsx
+++ app/components/random.tsx
@@ -4,16 +4,23 @@ import path from "path";
 import Image from "next/image";
 
 const Random = async () => {
-  const img = await sharp("public/photo.jpg");
+  const img = sharp("public/photo.jpg").resize({ width: 4096, height: 4096 });
+  const metadata = await img.metadata();
+  console.log(metadata);
+  const imageWidth = metadata.width || 4096;
+  const username = "gulzar";
 
   const random = await img
     .composite([
       {
         input: {
           text: {
-            text: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.`,
+            text: `<span foreground="white"  size="large">BYa <span foreground="#ff9800">go</span> AmO dmo JwµOao Vmo _w±h \\o$ao hwE JwµOao\nA~ CZ go ^r h_mar ~o-H$gr XoIr Zht OmVr</span>`,
             justify: true,
-            width: 100,
+            width: 2048,
+            height: 4096,
+            rgba: true,
+            align: "center",
             font: "SHREE-DEV7-1206 Regular",
             fontfile: path.join(
               process.cwd(),
@@ -22,9 +29,52 @@ const Random = async () => {
             wrap: "word",
           },
         },
+        gravity: "center",
+        // tile: true,
+      },
+
+      {
+        input: {
+          text: {
+            text: `<span  foreground="white">Shayari Page</span>`,
+            justify: true,
+            width: 1024,
+            height: 4096,
+            rgba: true,
+            align: "center",
+            font: "Katty Diona",
+            fontfile: path.join(process.cwd(), "public/kattydiona.otf"),
+            wrap: "word",
+          },
+        },
+        gravity: "south",
+        top: 4096 - 512,
+        left: Math.floor((imageWidth - 1024) / 2), // Center horizontally
+
+        // tile: true,
+      },
+      {
+        input: {
+          text: {
+            text: `<span foreground="white">shayaripage.com<i>/</i>@${username}</span>`,
+            justify: true,
+            width: 1024,
+            height: 4096,
+            rgba: true,
+            align: "center",
+            font: "Mukta",
+            fontfile: path.join(process.cwd(), "/public/Mukta-ExtraLight.ttf"),
+            wrap: "word",
+          },
+        },
+        gravity: "south",
+        top: 4096 - 200,
+        left: Math.floor((imageWidth - 1024) / 2), // Center horizontally
+
+        // tile: true,
       },
     ])
-    .jpeg()
+
     .toBuffer();
   console.log(
     path.join(process.cwd(), "/public/font/SHREE-DEV7-1206 Regular.ttf")
@@ -34,8 +84,8 @@ const Random = async () => {
     <Image
       src={`data:image/jpeg;base64,${random.toString("base64")}`}
       alt=""
-      width={400}
-      height={400}
+      width={800}
+      height={800}
     />
   );
 };
